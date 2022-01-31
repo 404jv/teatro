92de463ceaaeaee5c9689a32bca02475944c8cb8
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-01-31 14:08:33 -0300

    Create entity admin

---
+++ backend/src/entities/Admin.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('admins')
+class Admin {
+  
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  address: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Admin };
