27200a099113f8d019f9e964427feb9977979dd2
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-02 18:20:41 -0300

    Create routes to create participant

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import { AuthenticateAdminController } from './controllers/AuthenticateAdminController';
+import { CreateParticipantController } from './controllers/CreateParticipantController';
+import { ensureAdmin } from './middlewares/ensureAdmin';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const authenticateAdminController = new AuthenticateAdminController();
+const createParticipantController = new CreateParticipantController();
 
 const routes = Router();
 
 routes.post('/auth', authenticateAdminController.handle);
+
+routes.post('/participant/create',
+  ensureAuthenticated,
+  ensureAdmin,
+  createParticipantController.handle
+);
 
 export { routes };
