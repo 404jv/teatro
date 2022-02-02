580541f5b7d460f89d3632897ee370fab4d2b429
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-02 20:33:35 -0300

    Create migration to create table presentation

---
+++ backend/src/database/migrations/1643838510887-CreateTablePresentations.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateTablePresentations1643838510887 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'presentations',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+          },
+          {
+            name: 'localization',
+            type: 'varchar'
+          },
+          {
+            name: 'date',
+            type: 'timestamp'
+          },
+          {
+            name: 'hour',
+            type: 'varchar',
+          },
+          {
+            name: 'total_audience',
+            type: 'integer',
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('presentations');
+  }
+}
