b25fe70ee0d1a541ee050615883ff4d29cec1839
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-02 17:37:29 -0300

    use routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,32 @@
 import "reflect-metadata";
-import express from 'express';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import './database';
+import { routes } from "./routes";
+import { HttpError } from "./errors/HttpError";
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof HttpError) {
+    if (!error.message) {
+      return response.status(error.statusCode).end();
+    }
+
+    return response.status(error.statusCode).json({
+      error: error.message
+    })
+  }
+
+  console.log(error);
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+});
+
 
 app.listen(3333, () => console.log('🚀 Server is running at http://localhost:3000'));
