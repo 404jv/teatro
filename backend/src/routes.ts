c52dcf8255a8fd16ce60dbadb37bfb9eb94c0d63
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-02 21:59:43 -0300

    Refactor and route to create participation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { AuthenticateAdminController } from './controllers/AuthenticateAdminController';
 import { CreateParticipantController } from './controllers/CreateParticipantController';
+import { CreatePresentationController } from './controllers/CreatePresentationController';
 import { ListParticipantsController } from './controllers/ListParticipantsController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
@@ -8,20 +9,17 @@ import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 const authenticateAdminController = new AuthenticateAdminController();
 const createParticipantController = new CreateParticipantController();
 const listParticipantsController = new ListParticipantsController();
+const createPresentationController = new CreatePresentationController();
 
 const routes = Router();
 
 routes.post('/auth', authenticateAdminController.handle);
 
-routes.post('/participant/create',
-  ensureAuthenticated,
-  ensureAdmin,
-  createParticipantController.handle
-);
-routes.get('/participant',
-  ensureAuthenticated,
-  ensureAdmin,
-  listParticipantsController.handle
-)
+routes.use(ensureAuthenticated, ensureAdmin);
+
+routes.post('/participant/create', createParticipantController.handle);
+routes.get('/participant', listParticipantsController.handle);
+
+routes.post('/presentation/create', createPresentationController.handle);
 
 export { routes };
