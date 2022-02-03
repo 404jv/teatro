6511713a20e9b5d3c333107c7728c89cbd005a96
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-03 20:30:36 -0300

    Create entity Report

---
+++ backend/src/entities/Report.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('reports')
+class Report {
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  presentation_id: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  date: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Report };
