b4e8f4db47c04ab3a3023eef1a630d202daa1bc0
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-02 22:13:25 -0300

    add routes to list presentations

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateAdminController } from './controllers/AuthenticateAdminCont
 import { CreateParticipantController } from './controllers/CreateParticipantController';
 import { CreatePresentationController } from './controllers/CreatePresentationController';
 import { ListParticipantsController } from './controllers/ListParticipantsController';
+import { ListPresentationsController } from './controllers/ListPresentationsController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
@@ -10,16 +11,17 @@ const authenticateAdminController = new AuthenticateAdminController();
 const createParticipantController = new CreateParticipantController();
 const listParticipantsController = new ListParticipantsController();
 const createPresentationController = new CreatePresentationController();
+const listPresentationsController = new ListPresentationsController();
 
 const routes = Router();
 
 routes.post('/auth', authenticateAdminController.handle);
 
-routes.use(ensureAuthenticated, ensureAdmin);
+routes.get('/presentation', listPresentationsController.handle);
 
+routes.use(ensureAuthenticated, ensureAdmin);
 routes.post('/participant/create', createParticipantController.handle);
 routes.get('/participant', listParticipantsController.handle);
-
 routes.post('/presentation/create', createPresentationController.handle);
 
 export { routes };
